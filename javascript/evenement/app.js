1813a685464b20c813026c1b89cfc63ee38d4787
Sydney COHEN <chnsydney@gmail.com>
2017-04-05 12:55:47 +0200

    "event"

--- javascript/evenement/app.js
+++ javascript/evenement/app.js
@@ -8,14 +8,53 @@
 //     this.classList.remove('red')
 //   })
 // }
-var liens = document.querySelectorAll('a.external')
-for (var i = 0; i < liens.length; i++) {
-  var lien = liens[i]
-  lien.addEventListener('click', function(e){
-    var reponse = window.confirm("Es tu sur de vouloir quitter ce site internet?")
-    console.log(e)
-    if (reponse === false){
-      e.preventDefault
-    }
-  })
-}
+// var liens = document.querySelectorAll('a.external')
+// for (var i = 0; i < liens.length; i++) {
+//   var lien = liens[i]
+//   lien.addEventListener('click', function(event){
+//     event.stopPropagation()
+//     console.log("j'ai cliquer sur le lien", event)
+//     var reponse = window.confirm("Es tu sur de vouloir quitter ce site internet?")
+//     if (reponse === false){
+//       event.preventDefault()
+//     }
+//   })
+// }
+//
+// var p = document.querySelector('p')
+// var onClick = function(e){
+//   this.classList.add('red')
+//   console.log('red')
+//   e.preventDefault()
+//   p.removeEventListener('click', onClick)
+// }
+// p.addEventListener('click', onClick)
+
+// document.querySelector('#a').addEventListener("keydown", function(e){
+//   console.log(String.fromCharCode(e.keyCode))
+//   var lettre = String.fromCharCode(e.keyCode)
+//   if (lettre != 'A') {
+//     e.preventDefault()
+//   }
+// })
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+  var demo = document.querySelector('#demo')
+  console.log(demo.value)
+})
+// demo.focus()
+//
+// document.querySelector('#form').addEventListener('submit', function(e){
+//   // var mention = document.querySelector('#mention')
+//   // if (!mention.checked){
+//   //   alert('need to agree to cgu')
+//   //   e.preventDefault
+//   // }
+//   var age = parseInt(document.querySelector('#age').selectedOptions[0].value, 10)
+//   if (age < 18){
+//     alert('trop jeune')
+//     e.preventDefault()
+//   }
+// })
