3932654a6a55bc49d71d0673b52bd2306a894db0
Sydney COHEN <chnsydney@gmail.com>
2017-04-07 18:46:54 +0200

    "hello"

---
+++ calculator/app/components/Secret.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  TouchableHighlight
+} from 'react-native';
+import Style from '../style/Style'
+
+export default class SecretScreen extends Component {
+
+  render() {
+    return (
+      <View>
+       <Text>
+       Secret Screen
+       </Text>
+      </View>
+    )
+  }
+}
